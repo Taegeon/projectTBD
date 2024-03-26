80743db477a248c7254a9b780debd6db27a42134
Taegeon Lee <dlxorjs663@gmail.com>
2024-03-26 16:14:26 -0400

    finished up input components

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -3,15 +3,20 @@ import CommentItem from 'components/CommentItem';
 import Top3Item from 'components/Top3Item';
 import FavoriteItem from 'components/FavoriteItem';
 import { commentListMock, latestBoardListMock, top3BoardListMock, favoriteListMock } from 'mocks';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputBox from 'components/InputBox';
 
 function App() {
+
+  const [value, setValue] = useState<string>('');
+
+
+
   return (
     <>
 
-      <InputBox/>
+      <InputBox label = 'email' type = 'text' placeholder='please type the email' value={value} error={false} setValue={setValue}/>
 
       
     </>
--- board-front/src/components/InputBox/index.tsx
+++ board-front/src/components/InputBox/index.tsx
@@ -1,21 +1,53 @@
-import React, { forwardRef } from 'react'
+import React, { ChangeEvent, Dispatch, KeyboardEvent, forwardRef } from 'react'
 import './style.css'
 
 interface Props {
+    label: string;
+    type: 'text' | 'password';
+    placeholder: string;
+    value : string;
+    setValue : Dispatch<React.SetStateAction<string>>
+    error: boolean;
+
+    icon?: string;
+    onButtonClick?: () => void;
+
+    message?: string;
+
+    onKeyDown?:(event: KeyboardEvent<HTMLInputElement>) => void;
+
 
 }
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+
+
+    const {label, type, placeholder, value, error, icon, message} = props;
+    const {setValue, onButtonClick, onKeyDown} = props;
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setValue(value);
+    }
+
+    const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (!onKeyDown) return;
+        onKeyDown(event);
+    };
+
     return (
         <div className='inputbox'>
-                <div className='inputbox-label'> {'password*'}</div>
-                <div className='inputbox-container'> 
-                    <input className='input'/>
-                    <div className='icon-button'>
-                        <div className='icon eye-light-off-icon'></div>
-                    </div>
-                
+                <div className='inputbox-label'> {label}</div>
+                <div className= {error? 'inputbox-container-error' : 'inputbox-container'}> 
+                    <input ref={ref} type={type} className='input' placeholder={placeholder} value = {value} onChange={onChangeHandler} onKeyDown={onKeyDownHandler}/>
+                    {onButtonClick !== undefined&& (
+                        <div className='icon-button'>
+                            {icon !== undefined && <div className={`icon ${icon}`}></div>}
+                        </div>
+                    )}
+                    
                 </div>
-                <div className='inputbox-message'> {'password is longer than 8 char'}</div>
+                {message !== undefined && <div className='inputbox-message'> {'password is longer than 8 char'}</div>}
+                
         </div>
     )
 
