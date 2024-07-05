7bc1217c330e72726ba889a23a0f1854f05f4e11
Taegeon Lee <dlxorjs663@gmail.com>
2024-07-05 11:09:17 -0400

    77. upper user page component style&event handler

--- board-front/src/views/User/index.tsx
+++ board-front/src/views/User/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import './style.css'
 import DefaultProfileImage from 'assets/image/Default_pfp.png'
 import { useParams } from 'react-router-dom';
@@ -14,10 +14,35 @@ export default function User() {
     const [changeNickname, setChangeNickname] = useState<string>('');
     const [profileImage, setProfileImage] = useState<string | null>(null);
 
+    const onProfileBoxClickHandler = () => {
+      if (!isMyPage) return;
+      if (!imageInputRef.current) return;
+      imageInputRef.current.click();
+    }
+
+    const onNicknameEditButtonClickHandler = () => {
+      setChangeNickname(nickname);
+      setNicknameChange(!isNicknameChange);
+    }
+
+    const onProfileImageChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      if (!event.target.files || !event.target.files.length) return;
+
+      const file = event.target.files[0];
+      const data = new FormData();
+      data.append('file',file);
+    };
+
+    const onNicknameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      const {value} = event.target;
+      setChangeNickname(value);
+    }
+
+
     useEffect(() => {
       if(!userEmail) return;
       setNickname('임시닉네임');
-      setProfileImage('https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=1280&format=png&auto=webp&s=7177756d1f393b6e093596d06e1ba539f723264b');
+      setProfileImage('https://images.pexels.com/photos/3844788/pexels-photo-3844788.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
 
     }, [userEmail]);
 
@@ -26,17 +51,15 @@ export default function User() {
       <div id='user-top-wrapper'>
         <div className='user-top-container'>
           {isMyPage ?
-          <div className='user-top-my-profile-image-box'>
+          <div className='user-top-my-profile-image-box' onClick={onProfileBoxClickHandler}>
             {profileImage !== null ?
             <div className='user-top-profile-image' style={{backgroundImage: `url(${profileImage})`}}></div> :
-            <div className='user-top-my-profile-image-nothing-box'>
               <div className='icon-box-large'>
                 <div className='icon image-box-white-icon'></div>
               </div>
-            </div>
             }
             
-            <input ref={imageInputRef} type='file' accept='image/*' style={{display:'none'}}/>
+            <input ref={imageInputRef} type='file' accept='image/*' style={{display:'none'}} onChange={onProfileImageChangeHandler}/>
           </div> :
           <div className='user-top-profile-image-box' style={{backgroundImage: `url(${profileImage ? profileImage : DefaultProfileImage})`}}></div>
           }
@@ -46,11 +69,11 @@ export default function User() {
               {isMyPage ?
               <>
               {isNicknameChange ?
-                <input className='user-top-info-nickname-input' type='text' size={changeNickname.length + 1} value={changeNickname} /> :
+                <input className='user-top-info-nickname-input' type='text' size={changeNickname.length + 2} value={changeNickname} onChange={onNicknameChangeHandler}/> :
                 <div className='user-top-info-nickname'>{nickname}</div>
               }
               
-              <div className='icon-button'>
+              <div className='icon-button' onClick={onNicknameEditButtonClickHandler}>
                 <div className='icon edit-icon'></div>
               </div>
               
